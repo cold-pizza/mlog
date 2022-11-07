55f41f4e43fd0a119d4a0d34d0b13a70a21e4876
Jay <originxh@gmail.com>
2022-11-07 16:00:03 +0900

    first commit

---
+++ src/view/login/login.tsx
@@ -0,0 +1,5 @@
+const Login = function () {
+    return <div></div>;
+};
+
+export default Login;
---
+++ src/view/main/index.tsx
@@ -0,0 +1,7 @@
+import "./style.scss";
+
+const Main = function () {
+    return <div>main view</div>;
+};
+
+export default Main;
