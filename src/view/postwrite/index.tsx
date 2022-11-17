183603d4554e8898730551e75982ab7db2e76c0e
Jay <originxh@gmail.com>
2022-11-17 16:24:31 +0900

    add post-write view

--- src/view/postwrite/index.tsx
+++ src/view/postwrite/index.tsx
@@ -3,12 +3,13 @@ import Nav from "../nav";
 
 const PostWrite = function () {
     return (
-        <div className="post-wirte">
+        <div className="post-write">
             <Nav />
             <section className="post-write-box">
                 <input type="text" placeholder="제목을 입력하세요." />
                 <button>이미지</button>
-                <img src="#" alt="#" />
+                {/* 이미지 추가시 생성 */}
+                {/* <img src="#" alt="#" /> */}
                 <textarea placeholder="내용을 입력하세요."></textarea>
             </section>
         </div>
